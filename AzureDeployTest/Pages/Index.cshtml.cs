d4b744cccefa12fc3fe46317ea1e70e9969a220f
norihito.yamazakis <kizinn1224@gmail.com>
2023-01-16 23:11:11 +0900

    di httpclient

--- AzureDeployTest/Pages/Index.cshtml.cs
+++ AzureDeployTest/Pages/Index.cshtml.cs
@@ -1,20 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
 
 namespace AzureDeployTest.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IHttpClientFactory _httpClientFactory = null!;
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+#pragma warning disable CRRSP08 // A misspelled word has been found
+        public Todo[]? Todos = Array.Empty<Todo>();
+#pragma warning restore CRRSP08 // A misspelled word has been found
+
+        public IndexModel(
+        IHttpClientFactory httpClientFactory,
+        ILogger<IndexModel> logger) =>
+        (_httpClientFactory, _logger) = (httpClientFactory, logger);
+
+        public async Task<IActionResult> OnGetAsync(int userId =1)
         {
-            _logger = logger;
-        }
+            // Create the client
+            using HttpClient client = _httpClientFactory.CreateClient();
+            try
+            {
+                // Make HTTP GET request
+                // Parse JSON response deserialize into Todo types
+                Todos = await client.GetFromJsonAsync<Todo[]>(
+                    $"https://jsonplaceholder.typicode.com/todos?userId={userId}",
+                    new JsonSerializerOptions(JsonSerializerDefaults.Web));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error getting something fun to say: {Error}", ex);
+            }
+            return Page();
 
-        public void OnGet()
-        {
-
         }
+    }
+
+    public class Todo
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
     }
 }
